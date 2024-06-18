1675533e8ecbf60d5d31e2ef036efe126eec029c
Edgar Avetisyan <105380778+Avetisyan00@users.noreply.github.com>
2024-06-18 18:59:38 +0400

    tests

--- src/main/java/am/itspace/catalogueservice/controller/HomeController.java
+++ src/main/java/am/itspace/catalogueservice/controller/HomeController.java
@@ -1,4 +1,14 @@
 package am.itspace.catalogueservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+
+    @GetMapping("/")
+    public String greet() {
+        return "Hello";
+    }
+
 }
